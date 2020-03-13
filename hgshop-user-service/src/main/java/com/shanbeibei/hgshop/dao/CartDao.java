34d48614e1c74b331ed9281dee2e8c4f916eb222
shanbeibei <2926499468@qq.com>
2020-03-13 12:19:48 +0800

    first

--- hgshop-user-service/src/main/java/com/shanbeibei/hgshop/dao/CartDao.java
+++ hgshop-user-service/src/main/java/com/shanbeibei/hgshop/dao/CartDao.java
@@ -5,11 +5,11 @@ import java.util.List;
 import com.shanbeibei.hgshop.pojo.Cart;
 /**
  * 
- * @author zhuzg
+ * @author shanbeibei
  *
  */
 public interface CartDao {
-
+	
 	int delete(int[] ids);
 
 	int add(Cart cart);
@@ -20,7 +20,6 @@ public interface CartDao {
 
 	List<Cart> listByIds(int[] cartIds);
 	
-	//根据购物车数据id 数据
 	void deleteByIds(int[] cartIds);
 
 }
